586b47d3097a785692d7e87bfd6ea74a01f9cf1a
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-02-20 12:34:28 -0500

    Added current user to display in Sidebar component.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import registerServiceWorker from './registerServiceWorker';
 import { Provider} from 'react-redux';
 import { createStore } from 'redux';
 
+import { addUser } from './actions';
 import chat from './reducers';
 
 const store = createStore(chat);
+store.dispatch(addUser('Me'));
 
 ReactDOM.render(
     <Provider store={store}>
