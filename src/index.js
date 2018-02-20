d677f441a60b6e1b9a03edaab6d0af9ace32bf80
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-02-20 12:51:03 -0500

    Added server and client Web Socket support.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import { addUser } from './actions';
+import { setupSocket } from './sockets';
+
 import chat from './reducers';
 
 const store = createStore(chat);
